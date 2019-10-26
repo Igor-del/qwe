edcb36c94c047e18e8d629ec9f0b28b9a1e92a1f
Android User <Android User@omen-01.sttec.net>
2019-10-26 11:13:44 +0300

    ses2

---
+++ App2/App2/App2.Android/obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 6e2f257c3026b10f9dfaa0e7bcb19f0e7c13964f */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1284
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 336409
+	.include	"typemap.jm.inc"
